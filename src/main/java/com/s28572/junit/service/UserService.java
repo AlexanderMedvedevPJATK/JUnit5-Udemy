3f20768cff93e57a4e7e0c3362c5b0d0a1e2af6f
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-06-30 17:31:54 +0200

    new tests

--- src/main/java/com/s28572/junit/service/UserService.java
+++ src/main/java/com/s28572/junit/service/UserService.java
@@ -1,27 +1,32 @@
 package com.s28572.junit.service;
 
 import com.s28572.junit.dto.User;
+import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+@Getter
 public class UserService {
     
-    private List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
 
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void addUser(User user) {
-        users.add(user);
+    public void add(User... users) {
+        this.users.addAll(Arrays.asList(users));
     }
 
     public Optional<User> login(String username, String password) {
+        if (username == null || password == null) {
+            throw new IllegalArgumentException("Username or password is null");
+        }
+
         return users.stream()
                 .filter(user -> user.getUsername().equals(username) && user.getPassword().equals(password))
                 .findFirst();
 
+    }
+
+    public Map<Integer, User> getUsersMappedById() {
+        return users.stream()
+                .collect(HashMap::new, (map, user) -> map.put(user.getId(), user), HashMap::putAll);
     }
 }
--- src/test/java/com/s28572/junit/service/UserServiceTest.java
+++ src/test/java/com/s28572/junit/service/UserServiceTest.java
@@ -3,8 +3,10 @@ package com.s28572.junit.service;
 import com.s28572.junit.dto.User;
 import org.junit.jupiter.api.*;
 
+import java.util.Map;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 // PER_METHOD: A new test instance will be created for each test method. (Default)
@@ -14,6 +16,7 @@ public class UserServiceTest {
 
     private static final User ALEX = User.of(1, "Alexander", "password");
     private static final User JOHN = User.of(2, "John", "password");
+
     private UserService userService;
 
     @BeforeAll
@@ -34,22 +37,48 @@ public class UserServiceTest {
 
     @Test
     void usersSizeIfUserAdded() {
-        userService.addUser(ALEX);
-        userService.addUser(JOHN);
-        assertEquals(2, userService.getUsers().size(), "Expected list size 2");
+        userService.add(ALEX, JOHN);
+//        assertEquals(2, userService.getUsers().size(), "Expected list size 2");
+        assertThat(userService.getUsers()).hasSize(2);
+
     }
 
     @Test
     void loginSuccessIfUserExists() {
-        userService.addUser(ALEX);
-        Optional<User> user = userService.login("Alexander", "password");
+        userService.add(ALEX);
+        Optional<User> user = userService.login(ALEX.getUsername(), ALEX.getPassword());
 
-        user.ifPresent(usr -> assertEquals(ALEX, usr));
+//        assertTrue(user.isPresent());
+        assertThat(user).isPresent();
+//        user.ifPresent(usr -> assertEquals(ALEX, usr));
+        user.ifPresent(usr -> assertThat(usr).isEqualTo(ALEX));
+    }
+
+    @Test
+    void loginThrowsExceptionIfUserOrPasswordIsNull() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login("username", null)),
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login(null, "password")),
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login(null, null))
+        );
+    }
+
+    @Test
+    void usersMappedById() {
+        userService.add(ALEX, JOHN);
+
+        Map<Integer, User> usersById = userService.getUsersMappedById();
+
+
+        assertAll(
+                () -> assertThat(usersById).containsKeys(ALEX.getId(), JOHN.getId()),
+                () -> assertThat(usersById).containsValues(ALEX, JOHN)
+        );
     }
 
     @Test
     void loginFailIfPasswordIncorrect() {
-        userService.addUser(ALEX);
+        userService.add(ALEX);
         Optional<User> user = userService.login(ALEX.getUsername(), "wrong_password");
         assertTrue(user.isEmpty());
     }
